03b7caa42bb3726e0b3bf5dbd62840f609eb048a
SaMeSparts <132626666+SaMeSparts@users.noreply.github.com>
2024-11-30 22:50:42 -0600

    Se añadieron los cambios al programa principal

--- lista.h
+++ lista.h
@@ -13,12 +13,9 @@ using namespace std;
 class lista_material
 {
 private:
-    vector<Madera> maderas={"Blanda","Dura"};
-    vector<Piedra> piedras={"Sedimentaria","Ignea"};
-    vector<Piedra> piedra_ignea={"Granito", "Basalto"};
-    vector<Piedra> piedra_sedimentaria={"Arenisca", "Calisa"};
-    vector<Madera> madera_blanda={"Pino","Cedro"};
-    vector<Madera> madera_dura={"Roble","Cerezo"};
+    vector<Madera> maderas={Madera("Pino","Blanda","Madera",400),Madera("Cedro","Blanda","Madera",400),Madera("Roble","Dura","Madera",400),Madera("Cerezo","Dura","Madera",400)};
+    vector<Piedra> piedras={Piedra("Basalto","Ignea","Piedra",50),Piedra("Granito","Ignea","Piedra",50),Piedra("Arenisca","Sedimentaria","Piedra",50),Piedra("Calisa","Sedimentaria","Piedra",50)};
+
 public:
     lista_material(){
     }
@@ -62,84 +59,6 @@ public:
         }
         return valid;
     }
-
-    void agregar_madera_blanda(Madera nombre){
-        madera_blanda.push_back(nombre);
-
-    }
-    bool eliminar_madera_blanda(string nombre){
-        bool valid;
-        valid=0;
-        Madera maderitas;
-        for (size_t i = 0; i < madera_blanda.size(); i++){
-            maderitas=madera_blanda[i];
-            if (maderitas.get_madera()== nombre)
-            {
-                madera_blanda.erase(madera_blanda.begin()+(i));
-                break;
-            }            
-        }    
-        return valid;   
-    }
-
-    void agregar_madera_dura(Madera nombre){
-        madera_dura.push_back(nombre);
-
-    }
-    bool eliminar_madera_dura(string nombre){
-        bool valid;
-        valid=0;
-        Madera maderitas;
-        for (size_t i = 0; i < madera_dura.size(); i++){
-            maderitas=madera_dura[i];
-        
-            if (maderitas.get_madera()== nombre)
-            {
-                madera_dura.erase(madera_dura.begin()+(i));
-                break;
-            }                     
-        }    
-        return valid;   
-    }
-
-    void agregar_piedra_ignea(Piedra nombre){
-        piedra_ignea.push_back(nombre);
-        
-    }
-    bool eliminar_ignea(string nombre){
-        bool valid;
-        valid=0;
-        Piedra piedritas;
-        for (size_t i = 0; i < piedra_ignea.size(); i++){
-            piedritas=piedra_ignea[i];
-            if (piedritas.get_piedra()== nombre)
-            {
-                piedra_ignea.erase(piedra_ignea.begin()+(i));
-                break;
-            }
-        }       
-        return valid; 
-    }
-
-    void agregar_piedra_sedimentaria(Piedra nombre){
-        piedra_sedimentaria.push_back(nombre);
-        
-    }
-
-    bool eliminar_piedra_sedimentaria(string nombre){
-        bool valid;
-        valid=0;
-        Piedra piedritas;
-        for (size_t i = 0; i < piedra_sedimentaria.size(); i++){
-            piedritas=piedra_sedimentaria[i];
-            if (piedritas.get_piedra()== nombre)
-            {
-                piedra_sedimentaria.erase(piedra_sedimentaria.begin()+(i));
-                break;
-            }             
-        }        
-        return valid;
-    }
     void imprimir_maderas(){
         Madera maderitas;
         cout << "Lista de maderas:" << endl;
@@ -154,9 +73,9 @@ public:
     void imprimir_madera_blanda(){
         Madera maderitas;
         cout << "Lista de maderas:" << endl;
-        for (size_t i = 0; i < madera_blanda.size(); i++)
+        for (size_t i = 0; i < maderas.size(); i++)
         {
-            maderitas=madera_blanda[i];
+            maderitas=maderas[i];
             cout << "" << maderitas.get_madera() << endl;
         }
         
@@ -165,9 +84,9 @@ public:
     void imprimir_madera_dura(){
         Madera maderitas;
         cout << "Lista de maderas:" << endl;
-        for (size_t i = 0; i < madera_dura.size(); i++)
+        for (size_t i = 0; i < maderas.size(); i++)
         {
-            maderitas=madera_dura[i];
+            maderitas=maderas[i];
             cout << "" << maderitas.get_madera() << endl;
         }
         
@@ -187,9 +106,9 @@ public:
     void imprimir_piedra_ignea(){
         Piedra piedritas;
         cout << "Lista de piedras:" << endl;
-        for (size_t i = 0; i < piedra_ignea.size(); i++)
+        for (size_t i = 0; i < piedras.size(); i++)
         {
-            piedritas=piedra_ignea[i];
+            piedritas=piedras[i];
             cout << "" << piedritas.get_piedra() << endl;
         }
         
@@ -198,9 +117,9 @@ public:
     void imprimir_piedra_sedimentaria(){
         Piedra piedritas;
         cout << "Lista de piedras:" << endl;
-        for (size_t i = 0; i < piedra_sedimentaria.size(); i++)
+        for (size_t i = 0; i < piedras.size(); i++)
         {
-            piedritas=piedra_sedimentaria[i];
+            piedritas=piedras[i];
             cout << "" << piedritas.get_piedra() << endl;
         }
         
@@ -209,5 +128,4 @@ public:
     
     
 };
-
 #endif LISTA_MATERIAL.H
